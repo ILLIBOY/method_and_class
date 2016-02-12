6bb05cea17735a369ffa0246a789f99e44e363af
ILLIBOY <illionairedan@ssem.re.kr>
2016-02-12 14:21:39 +0900

    call by reference
    주소값을 전달해줘서 retrun 하지 않아도 값이 바뀐다. 오 신기하다.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace method_and_class
+{
+	class MainClass
+	{
+
+		static void swap(ref int a, ref int b)
+		{
+			int temp = b;
+			b = a;
+			a = temp;
+		}
+
+		public static void Main (string[] args)
+		{
+			int x = 3;
+			int y = 4;
+
+			Console.WriteLine ("x:{0}, y:{1}", x, y);
+
+			swap (ref x, ref y);
+
+			Console.WriteLine ("x:{0}, y:{1}", x, y);
+		}
+	}
+}
